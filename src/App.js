746abc96e823aff4e5aeab640f7e58900bef657b
GMKmani <manikumar81796@gmail.com>
2023-04-12 19:11:44 +0530

    mani

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
+import Category from "./components/Category";
 import Main from "./components/Main";
-import Navbar from "./components/nav";
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="App">
      <Navbar/>
      <Main/>
+     <Category/>
     </div>
   );
 }
