ac94f5163c2c2c18d22906beaae5df9c8d842b32
MaheshS08 <mahesh.sambharam@gmail.com>
2023-04-12 18:31:20 +0530

    On Main.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Main from "./components/Main";
 import Navbar from "./components/nav";
 function App() {
   return (
     <div className="App">
      <Navbar/>
+     <Main/>
     </div>
   );
 }
