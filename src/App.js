00df588816103d634dd8c6b144af1cad6a8e591c
GMKmani <manikumar81796@gmail.com>
2023-04-12 19:43:17 +0530

    practice 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
 import Category from "./components/Category";
-import Main from "./components/Main";
 import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="App">
-     <Navbar/>
-     <Main/>
-     <Category/>
+      <Navbar />
+      <Category />
     </div>
   );
 }
